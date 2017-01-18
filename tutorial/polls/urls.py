c0c9751e8c175585a3d1e378d07e0497447b789d
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2017-01-18 09:30:27 -0800

    updating views and templates

--- tutorial/polls/urls.py
+++ tutorial/polls/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+# from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
-    url(r'^polls/', include('polls.urls')),
-    url(r'^admin/', admin.site.urls),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello world. You're at the polls index.")
+    latest_question_list = Question.objects.order_boy('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question {}.".format(question_id))
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question {}".format(question_id)
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}".format(question_id))
